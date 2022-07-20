23a6e6f6df2ac11024868c2d82d8fd2295b89d69
westbanana <93942346+westbanana@users.noreply.github.com>
2022-07-20 19:39:01 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import s from './App.module.scss';
 
 const App = () => {
   return (
-    <div className="App">
+    <div className={s.App}>
+      <div className={s.test}>
+        1231231
+      </div>
     </div>
   );
 }
